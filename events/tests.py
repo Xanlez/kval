ff199e43f57e620181cc616abe14286eb52b41fb
Xanlez <130233037+Xanlez@users.noreply.github.com>
2026-06-13 13:18:14 +0300

    Delete events directory

--- events/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class MainPageIntegrationTest(TestCase):
-    def test_main_page_returns_200(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
